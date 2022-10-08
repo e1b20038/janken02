4bb86438d2a3c6b13ef4381cbe9e04f323192c24
e1b20038 <e1b20038@oit.ac.jp>
2022-10-08 12:40:44 +0900

    initialize repositry

---
+++ src/main/java/oit/is/lec02/alone/janken02/Janken02Application.java
@@ -0,0 +1,13 @@
+package oit.is.lec02.alone.janken02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken02Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec02/alone/janken02/Janken02ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec02.alone.janken02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
